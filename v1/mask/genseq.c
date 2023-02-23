056dfc1c456ecdef572ababd3a045a7bd7bbe3ae
jacky <martin.pecheur@riviere.kwaii>
2023-02-23 11:37:57 +0100

    remplace u64 - u8 remplace double float et debut de conversion atcg 000...

--- v1/mask/genseq.c
+++ v1/mask/genseq.c
@@ -23,7 +23,9 @@ int main(int argc, char **argv)
   unsigned long long len = atoll(argv[2]);
   
   //
-  const char bases[4] = "ATCG"; 
+  // const char bases[4] = "ATCG"; 
+  // DONE compression
+  const char bases[4] = "00011011"; 
   
   //
   FILE *fp = fopen(argv[1], "wb");
@@ -31,10 +33,14 @@ int main(int argc, char **argv)
   if (!fp)
     return printf("Error: cannot create file '%s'\n", argv[2]), 2;
 
+  u8 rng;
   //Generate random DNA sequence
-  for (unsigned long long i = 0; i < len; i++)
-    fprintf(fp, "%c", bases[randxy(0, 4)]);
-
+  // DONE modif type A -> 00, etc
+  for (unsigned long long i = 0; i < len; i++){
+    rng = randxy(0, 4)*2
+    printf(fp, "%c", bases[rng:rng+1]);
+    fprintf(fp, "%c", bases[rng:rng+1]);
+  }
   //Newline at EOF
   fprintf(fp, "\n");
   
